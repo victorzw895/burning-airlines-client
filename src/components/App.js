724bfa096470bd1d0a8c4e5042110ba581f0c1f5
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-13 21:48:23 +1000

    created search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchNilana from './SearchNilana';
 
 function App() {
   return (
     <div className="App">
       Burning airlines Coming soon
+      <SearchNilana />
     </div>
   );
 }
--- src/components/SearchNilana.js
+++ src/components/SearchNilana.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-// const SERVER_URL = 'http://localhost:3000/secrets.json';
+
 
 class SearchNilana extends Component {
+    state = {
+        items: [],
+        origin: '',
+        destination: '',
+    };
+    fetchFligthsByOriginAndDestination = () => {
+        const { origin, destination } = this.state;
+        // if origin is empty alert with message 'you should inform the book origin'
+        if (origin === '' || destination === '') {
+            alert('Your should inform the origin and destination');
+            return;
+        };
+
+        axios.get(`http://localhost:3000/search/${origin}/${destination}`).then((response) => {
+
+            this.setState({ items: response.data });
+        });
+
+    };
+
+    setOrigin = (event) => {
+        this.setState({ origin: event.target.value })
+    }
+    setDestination = (event) => {
+        this.setState({ destination: event.target.value })
+    }
+
+
     render() {
+        const { items } = this.state;
         return (
             <div>
-                <h1>Search Flight</h1>
-                <SearchForm /*onSubmit={ this.saveSecret } *//>
-                <FlightList /*secrets={ this.state.secrets }*/ />
-            </div>
-        );
-    }
-}
+                <p className="origin">Search</p>
+                <input className="search" type="search" placeholder="search by origin" onInput={this.setOrigin} />
+                <input className="search" type="search" placeholder="search by destination" onInput={this.setDestination} />
+                <button className="submit" type="button" onClick={this.fetchFligthsByOriginAndDestination}>Search</button>
 
-class SearchForm extends Component {
-    render() {
-        return (
-            <form>
- 
-            </form>
-        );
-    }
-}
-
-class FlightList extends Component {
-    render() {
-        return(
-            <div>
+                {/* loop in react to add images */}
+                {items.map((item) => (
+                    <div>
+                        <p>Date: {item.date}</p>
+                        <p>Flight: {item.flight}</p>
+                        <p>Origin: {item.origin}</p>
+                        <p>Destination: {item.destination}</p>
+                        <p>Airplane: {item.airplane}</p>
+                    </div>
+                ))}
             </div>
-        );
+
+
+        )
     }
 }
 
