01563364bdec96759f73fe8099e7d560b9d21b2b
Victor Zhang <victorzw895@gmail.com>
2019-08-13 17:36:00 +1000

    components search for flights view

---
+++ src/components/SearchNilana.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// const SERVER_URL = 'http://localhost:3000/secrets.json';
+
+class SearchNilana extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Search Flight</h1>
+                <SearchForm /*onSubmit={ this.saveSecret } *//>
+                <FlightList /*secrets={ this.state.secrets }*/ />
+            </div>
+        );
+    }
+}
+
+class SearchForm extends Component {
+    render() {
+        return (
+            <form>
+ 
+            </form>
+        );
+    }
+}
+
+class FlightList extends Component {
+    render() {
+        return(
+            <div>
+            </div>
+        );
+    }
+}
+
+export default SearchNilana;
