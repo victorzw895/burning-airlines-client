87542bda72fb5524e36fe6b2434ae605b11cde7d
Victor Zhang <victorzw895@gmail.com>
2019-08-14 10:07:46 +1000

    fixed typo on flights page from users to admin

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <p><Link to="/airplanes">View Airplanes</Link></p>
+                <p><Link to="/">Back Home</Link></p>
+            </div>
+        )
+    }
+}
+
+class CreateFlight extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <h2>Burning Airlines</h2>
+                    <input type="submit" value="Create"/>
+
+                    <input type="text"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+
 class Flights extends Component {
     render() {
         return (
             <div>
-                <h2>This is the Flights page, users select flights and book flights</h2>
-                <p><Link to="/airplanes">View Airplanes</Link></p>
-                <p><Link to="/">Back Home</Link></p>
+                <Navigation />
+                <CreateFlight />
+                <h2>This is the Flights page, Admin select flights and book flights</h2>
+
             </div>
         )
     };
